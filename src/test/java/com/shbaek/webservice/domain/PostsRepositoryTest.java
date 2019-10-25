29a872b7eba422efc0b4bddff95d7b7e5ebba056
shabek <asmita.bella@gmail.com>
2019-10-25 22:04:05 +0900

    스프링부트로 웹 서비스 출시하기 - 2. SpringBoot & JPA로 간단 API 만들기

--- src/test/java/com/shbaek/webservice/domain/PostsRepositoryTest.java
+++ src/test/java/com/shbaek/webservice/domain/PostsRepositoryTest.java
@@ -9,10 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 
 @RunWith(SpringRunner.class)
@@ -30,21 +32,43 @@ public class PostsRepositoryTest {
     @Test
     public void 게시물저장_불러오기() {
 
-//        arrange
+        // arrange
         postsRepository.save(Posts.builder()
                 .title("첫게시물")
                 .content("첫 게시물 내용!!!")
                 .author("shbaek")
                 .build());
 
-//      action
+        // action
         List<Posts> postsList = postsRepository.findAll();
 
 
-//      assert
+        // assert
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle(), is("첫게시물"));
         assertThat(posts.getContent(), is("첫 게시물 내용!!!"));
+
+    }
+
+    @Test
+    public void BaseTimeEntity_등록(){
+
+        // arrange
+        LocalDateTime now = LocalDateTime.now();
+        postsRepository.save(Posts.builder()
+                .title("테스트 게시물")
+                .content("테스트 게스글")
+                .author("shbaek@google.com")
+                .build());
+
+        // action
+        List<Posts> postsList = postsRepository.findAll();
+
+
+        // assert
+        Posts posts = postsList.get(0);
+        assertTrue(posts.getCreateDate().isAfter(now));
+        assertTrue(posts.getUpdateDate().isAfter(now));
 
     }
 }
