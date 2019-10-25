319d0656258939a3d441362bd4ee0a3b178f8e6d
shabek <asmita.bella@gmail.com>
2019-10-25 20:16:10 +0900

    프로젝트 생성

---
+++ src/test/java/com/shbaek/webservice/domain/PostsRepositoryTest.java
@@ -0,0 +1,50 @@
+package com.shbaek.webservice.domain;
+
+import com.shbaek.webservice.domain.posts.Posts;
+import com.shbaek.webservice.domain.posts.PostsRepository;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시물저장_불러오기() {
+
+//        arrange
+        postsRepository.save(Posts.builder()
+                .title("첫게시물")
+                .content("첫 게시물 내용!!!")
+                .author("shbaek")
+                .build());
+
+//      action
+        List<Posts> postsList = postsRepository.findAll();
+
+
+//      assert
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle(), is("첫게시물"));
+        assertThat(posts.getContent(), is("첫 게시물 내용!!!"));
+
+    }
+}
